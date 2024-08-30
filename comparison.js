043ee4962d89a342f456d55adef92fe2abc71bb1
mizansanzi <pleasemailmizan@gmail.com>
2024-08-30 14:41:53 +0600

    first commit

---
+++ comparison.js
@@ -0,0 +1,21 @@
+// comparison of numbers
+console.log(5>10);
+console.log(10>5);
+console.log(5 == 10);
+console.log(10 == 10);
+console.log(10 >= 10);
+console.log(15>=5);
+
+// comparison of variable
+var applePrice = 250;
+var bananaPrice = 50;
+
+console.log(applePrice<bananaPrice);
+
+// more comparison
+if (5>7){
+    console.log("what? false!");
+}
+else{
+    console.log("Wrong Comparison");
+}
